ebc90806f83b0104528c302ddf371aa3f36acaea
srikaranbaradhwaj <skbskb1999@gmail.com>
2020-12-18 12:08:30 +0530

    multiply 2

--- git-eclipse/src/sampGit.java
+++ git-eclipse/src/sampGit.java
@@ -4,7 +4,7 @@ public class sampGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=2,b=5;
-		System.out.println(a+b);
+		System.out.println(a*b*a);
 		System.out.println("Helloo");
 		
 	}
