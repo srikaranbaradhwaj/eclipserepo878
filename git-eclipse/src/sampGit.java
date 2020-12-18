6e9ab27507c5fa02e68e16f1c8d56aa769899825
srikaranbaradhwaj <skbskb1999@gmail.com>
2020-12-18 11:59:59 +0530

    changed value of b

--- git-eclipse/src/sampGit.java
+++ git-eclipse/src/sampGit.java
@@ -3,7 +3,7 @@ public class sampGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=2,b=3;
+		int a=2,b=5;
 		System.out.println(a+b);
 		System.out.println("Helloo");
 		
